429e72478cf9f6704d6db72e978fdd7f4c8adab9
Infalir <irvik005@gmail.com>
2026-03-08 21:42:24 +0300

    Initial commit

---
+++ src/test/java/com/example/application/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.application;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
